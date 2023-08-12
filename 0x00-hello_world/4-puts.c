12af895e83196755c3bfcb5bc3a7cf7131ce9eb5
072san <musandiwamavhngu063@gmail.com>
2023-08-12 11:58:11 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+!#/bin/bash
+#include <stdio.h>
+
+/**
+ * main - taking this as the main
+ *
+ * Return: will be 0
+ */
+
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
+
